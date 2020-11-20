7a74353314a06cbaf25a981ff352bb852efd5ca1
andre <amzingdudu8@gmail.com>
2020-11-20 20:39:38 +0700

    add todo controller

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use App\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $active = 1;
+        $data = Todo::where('status_id', '=', $active);
+
+        return view('todo.today', compact('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('todo.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $this->validate($request, [
+            'name' => 'required',
+            'category' => 'required',
+            'deadline' => 'nullable|date',
+            'isImportant' => 'required',
+            'notes' => 'nullable'
+        ]);
+
+        // tfor testing only
+        if ($request->category == 'Web Development') {
+            $category_id = 1;
+        }
+
+        // $category_id = Category::where('name', '=', $request->category);
+
+        Todo::create(array_merge(
+            $data,
+            ['user_id' => auth()->user()->id],
+            ['category_id' => $category_id]
+        ));
+
+        return redirect()->route('todo.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo)
+    {
+        //
+    }
+}
