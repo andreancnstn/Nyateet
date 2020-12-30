159b50dd0ac9a44e982fae081867c71c1bf71267
KevinRus <kevinrusmin20@gmail.com>
2020-12-30 22:27:21 +0700

    Homepage done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.homeLayout')
 
 @section('content')
-    HALO IN HOMEPAGE
+    <div class="container-md">
+        <div id="myCarousel" class="carousel slide img-fluid" data-interval="3000" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="../assets/img/wallpaper/wall1inv.png" alt="First Slide">
+                </div>
+                <div class="carousel-item">
+                    <img src="../assets/img/wallpaper/wall2inv.png" alt="Second Slide">
+                </div>
+                <div class="carousel-item">
+                    <img src="../assets/img/wallpaper/wall3inv.png" alt="Third Slide">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
 @endsection
