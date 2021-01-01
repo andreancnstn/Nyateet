d6c558b17193c5e7e0ce631bd5281fb3e7d77824
andreancnstn <66414794+andreancnstn@users.noreply.github.com>
2021-01-01 20:37:35 +0700

    fixed mixed block

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
 
     'url' => env('APP_URL', 'https://nyateet.herokuapp.com/'),
 
-    'asset_url' => env('ASSET_URL', 'https://nyateet.herokuapp.com'),
+    'asset_url' => env('ASSET_URL', null),
 
     /*
     |--------------------------------------------------------------------------
