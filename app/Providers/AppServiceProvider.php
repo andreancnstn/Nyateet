1d184b8cc1a6ff7bbd23b612100bb15f4c9f7c90
andreancnstn <66414794+andreancnstn@users.noreply.github.com>
2021-01-07 14:26:11 +0700

    change boot to local

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forceScheme('https');
+        //
+        // only for deploy
+        // URL::forceScheme('https');
     }
 }
